a4bb15e9eb24674b9f429bc88758a6cdb47f5704
rigor789 <rigor789@gmail.com>
2017-06-02 12:51:56 +0100

    Stared working on the ActionBar

---
+++ nativescript-vue/platform/nativescript/runtime/components/action-bar.js
@@ -0,0 +1,15 @@
+export default {
+    name: 'action-bar',
+
+    template: `<native-action-bar></native-action-bar>`,
+
+    props: {},
+
+    created() {
+    },
+
+    mounted() {
+    },
+
+    methods: {}
+}
