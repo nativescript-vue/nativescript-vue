63c61a6f9646335c4487c94a13c5da1704bfc398
rigor789 <rigor789@gmail.com>
2017-10-01 13:14:28 +0100

    Playing around with the action bar

--- samples/app/app-with-tab-view.js
+++ samples/app/app-with-tab-view.js
@@ -1,20 +1,21 @@
 const Vue = require('./nativescript-vue')
 
 let app = new Vue({
-  data: {
-    selectedTab: 0,
-    tabs: [
-      { title: 'First Tab', text: 'im the first tab' },
-      { title: 'Second Tab', text: 'im the second tab' },
-      { title: 'Third Tab', text: 'im the third tab' }
-    ]
-  },
+    data: {
+        selectedTab: 0,
+        tabs: [
+            { title: 'First Tab', text: 'im the first tab' },
+            { title: 'Second Tab', text: 'im the second tab' },
+            { title: 'Third Tab', text: 'im the third tab' }
+        ]
+    },
 
-  template: `
+    template: `
         <page>
             <action-bar title="test">
               <stack-layout orientation="horizontal" horizontalAlignment="center">
-                <Label style="font-size: 20">
+                <image src="res://icon" style="width: 30; height: 30; vertical-align: center; margin-right: 10;"/>
+                <Label style="font-size: 20; vertical-align: center;">
                   <FormattedString>
                     <Span text="Tab" fontWeight="Bold"></Span>
                     <Span text="Views"></Span>
