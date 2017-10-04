a7f0e0599d67cb352d9b1e51c3137e7b0c9fc1ff
rigor789 <rigor789@gmail.com>
2017-10-04 23:33:23 +0100

    Add npm command

--- release.sh
+++ release.sh
@@ -1,23 +1,23 @@
 set -e
 
 if [[ -z $1 ]]; then
-    echo "Enter release type (major, minor, patch): "
-    read TYPE
+  echo "Enter release type (major, minor, patch): "
+  read TYPE
 else
-    TYPE = patch
+  TYPE = patch
 fi
 
 read -p "Releasing a $TYPE version - are you sure? (y/n) " -n 1 -r
 echo
 
 if [[ $REPLY == ^[Yy]$ ]]; then
-    echo "Releasing a $TYPE version..."
+  echo "Releasing a $TYPE version..."
 
-    # Build and Version
-    npm version $TYPE -m "[Release] %s"
+  # Build and Version
+  npm version $TYPE -m "[Release] %s"
 
-    # Publish
-    npm publish
+  # Publish
+  npm publish
 fi
 
 
