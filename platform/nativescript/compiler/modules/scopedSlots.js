8f3593b3d673efda81d6d3895dc0706b344d0595
rigor789 <rigor789@gmail.com>
2017-10-21 01:33:56 +0100

    Rename template scope to slot-scope internally, because scope is deprecated

--- platform/nativescript/compiler/modules/scopedSlots.js
+++ platform/nativescript/compiler/modules/scopedSlots.js
@@ -1,3 +1,5 @@
+import { normalizeElementName } from '../../element-registry'
+
 function preTransformNode(el, options) {
   if (el.tag === 'template') {
     let name = el.attrsMap.name
@@ -9,8 +11,22 @@ function preTransformNode(el, options) {
         value: name
       })
     }
+
+    let isListView = normalizeElementName(el.parent.tag) === 'listview'
+    let scope = el.attrsMap.scope
+    if (scope && isListView) {
+      delete el.attrsMap.scope
+      el.attrsList = el.attrsList.filter(attr => attr.name !== 'scope')
+
+      el.attrsMap['slot-scope'] = scope
+      el.attrsList.push({
+        name: 'slot-scope',
+        value: scope
+      })
+    }
   }
 }
+
 export default {
   preTransformNode
 }
