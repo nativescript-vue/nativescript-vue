462d0628434451ef098cba2bcbcb0f0191ec61b6
rigor789 <rigor789@gmail.com>
2017-11-10 12:48:17 +0000

    Add action bar to drawer example

--- samples/app/app-with-view-directive.js
+++ samples/app/app-with-view-directive.js
@@ -6,14 +6,19 @@ Vue.registerElement(
 
 new Vue({
   template: `
-    <RadSideDrawer ref="drawer">
-      <StackLayout ~drawerContent backgroundColor="white">
-        <Label text="Drawer" />
-      </StackLayout>
-      <StackLayout ~mainContent >
-        <Label text="Main" />
-        <Button text="Open Drawer" @tap="$refs.drawer.nativeView.showDrawer()"/>
-      </StackLayout>
-    </RadSideDrawer>
+    <Page>
+      <ActionBar text="Drawer Sample">
+          <ActionItem text="Menu" @tap="$refs.drawer.nativeView.showDrawer()"/>
+      </ActionBar>  
+      <RadSideDrawer ref="drawer">
+        <StackLayout ~drawerContent backgroundColor="white">
+          <Label text="Drawer" />
+        </StackLayout>
+        <StackLayout ~mainContent >
+          <Label text="Main" />
+          <Button text="Open Drawer" @tap="$refs.drawer.nativeView.showDrawer()"/>
+        </StackLayout>
+      </RadSideDrawer>
+    </Page>
   `
 }).$start()
