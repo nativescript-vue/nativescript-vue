dd74d2fbcc99c1d15fa37aae57876dcbb6155abe
rigor789 <rigor789@gmail.com>
2017-12-13 22:27:36 +0000

    fix: assign template bag to current instance

--- platform/nativescript/runtime/components/v-template.js
+++ platform/nativescript/runtime/components/v-template.js
@@ -21,8 +21,9 @@ export default {
       return
     }
 
-    this.$parent.$templates = this.$parent.$templates || new TemplateBag()
-    this.$parent.$templates.registerTemplate(
+    this.$templates = this.$parent.$templates =
+      this.$parent.$templates || new TemplateBag()
+    this.$templates.registerTemplate(
       this.$props.name || (this.$props.if ? `v-template-${tid++}` : 'default'),
       this.$props.if,
       this.$scopedSlots.default
