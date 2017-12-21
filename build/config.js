e40d3bde8fc87be8478f83e0208ed9e008ba6e13
rigor789 <rigor789@gmail.com>
2017-12-21 12:50:02 +0000

    chore: clean up rollup config, and use node-resolve

--- build/config.js
+++ build/config.js
@@ -1,5 +1,6 @@
 const alias = require('rollup-plugin-alias')
 const commonjs = require('rollup-plugin-commonjs')
+const resolve = require('rollup-plugin-node-resolve')
 const buble = require('rollup-plugin-buble')
 const replace = require('rollup-plugin-replace')
 const flow = require('rollup-plugin-flow-no-whitespace')
@@ -17,85 +18,77 @@ const banner = (name, version) => `
  */
 `
 const resolveVue = p => {
-  return path.resolve(process.cwd(), 'node_modules', 'vue/src/', p) + '/'
+    return path.resolve(process.cwd(), 'node_modules', 'vue/src/', p) + '/'
 }
 const aliases = {
-  vue: resolveVue('core/index'),
-  compiler: resolveVue('compiler'),
-  web: resolveVue('platforms/web'),
-  core: resolveVue('core'),
-  shared: resolveVue('shared'),
-  sfc: resolveVue('sfc'),
-  //he: path.resolve(__dirname, 'node_modules', 'he', 'he')
-  he: path.resolve(process.cwd(), 'platform/nativescript/util/entity-decoder')
+    vue: resolveVue('core/index'),
+    compiler: resolveVue('compiler'),
+    web: resolveVue('platforms/web'),
+    core: resolveVue('core'),
+    shared: resolveVue('shared'),
+    sfc: resolveVue('sfc'),
+    //he: path.resolve(__dirname, 'node_modules', 'he', 'he')
+    he: path.resolve(__dirname, '..', 'platform/nativescript/util/entity-decoder')
 }
 
 const builds = {
-  'nativescript-vue': {
-    entry: './platform/nativescript/framework.js',
-    dest: './dist/index.js',
-    moduleName: 'NativeScript-Vue',
-    banner: banner('NativeScript-Vue'),
-    external(id) {
-      return id.startsWith('ui/') || id.startsWith('application')
+    'nativescript-vue': {
+        entry: './platform/nativescript/framework.js',
+        dest: './dist/index.js',
+        moduleName: 'NativeScript-Vue',
+        banner: banner('NativeScript-Vue'),
+        external(id) {
+            return id.startsWith('ui/') || id.startsWith('application')
+        }
+    },
+    'nativescript-vue-template-compiler': {
+        entry: './platform/nativescript/compiler.js',
+        dest: './packages/nativescript-vue-template-compiler/index.js',
+        moduleName: 'NativeScript-Vue-Template-Compiler',
+        banner: banner('NativeScript-Vue-Template-Compiler'),
+        external: Object.keys(require('../packages/nativescript-vue-template-compiler/package.json').dependencies)
     }
-  },
-  'nativescript-vue-template-compiler': {
-    entry: './platform/nativescript/compiler.js',
-    dest: './packages/nativescript-vue-template-compiler/index.js',
-    moduleName: 'NativeScript-Vue-Template-Compiler',
-    banner: banner('NativeScript-Vue-Template-Compiler'),
-    external: Object.keys(require('../packages/nativescript-vue-template-compiler/package.json').dependencies)
-  }
 }
 
 
 const genConfig = (name) => {
-  const opts = builds[name]
-  const config = {
-    input: opts.entry,
-    external: opts.external,
-    output: {
-      file: opts.dest,
-      format: opts.format || 'cjs',
-      banner: opts.banner,
-      name: opts.moduleName
-    },
-    plugins: [
-      replace({
-        __WEEX__: false,
-        __VERSION__: VueVersion,
-        'process.env.NODE_ENV': "'development'",
-        'let _isServer': 'let _isServer = false',
-        'process.env.VUE_VERSION': `'${VueVersion}'`,
-        'process.env.NS_VUE_VERSION': `'${NSVueVersion}'`
-      }),
-      flow(),
-      buble(),
-      alias(aliases),
-      commonjs({
-        include: [
-          path.resolve(__dirname, 'node_modules', 'vue') + '/**',
-          //path.resolve(__dirname, 'node_modules', 'he') + '/**',
+    const opts = builds[name]
+    const config = {
+        input: opts.entry,
+        external: opts.external,
+        output: {
+            file: opts.dest,
+            format: opts.format || 'cjs',
+            banner: opts.banner,
+            name: opts.moduleName
+        },
+        plugins: [
+            replace({
+                __WEEX__: false,
+                __VERSION__: VueVersion,
+                'process.env.NODE_ENV': "'development'",
+                'let _isServer': 'let _isServer = false',
+                'process.env.VUE_VERSION': `'${VueVersion}'`,
+                'process.env.NS_VUE_VERSION': `'${NSVueVersion}'`
+            }),
+            flow(),
+            buble(),
+            alias(aliases),
+            resolve(),
+            commonjs(),
         ],
+    }
 
-        namedExports: {
-          'he': ['decode']
-        }
-      }),
-    ],
-  }
+    Object.defineProperty(config, '_name', {
+        enumerable: false,
+        value: name
+    })
 
-  Object.defineProperty(config, '_name', {
-    enumerable: false,
-    value: name
-  })
-
-  return config;
+    return config;
 }
 
 if (process.env.TARGET) {
-  module.exports = genConfig(process.env.TARGET)
+    module.exports = genConfig(process.env.TARGET)
 } else {
-  module.exports.getAllBuilds = () => Object.keys(builds).map(genConfig)
+    module.exports.getAllBuilds = () => Object.keys(builds).map(genConfig)
 }
