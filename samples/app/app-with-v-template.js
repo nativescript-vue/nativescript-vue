fb1d8c6a7d293066b4cb85d5e13286c4ec6b2f7e
rigor789 <rigor789@gmail.com>
2017-12-22 10:04:34 +0000

    refactor: move deepProxy into utils.

--- samples/app/app-with-v-template.js
+++ samples/app/app-with-v-template.js
@@ -31,25 +31,25 @@ new Vue({
   },
   template: `
     <Page>
-        <StackLayout>
+        <GridLayout>
             <list-view for="line in items">
                 <v-template>
                     <StackLayout style="padding: 50;">
                         <Label :text="line" style="color: blue;"/>
                     </StackLayout>
                 </v-template>
-                <v-template if="line.value.length == 2">
+                <v-template if="line.length == 2">
                     <StackLayout style="padding: 50;">
                         <Label :text="line" style="color: green;"/>
                     </StackLayout>
                 </v-template>
-                <v-template if="line.odd">
+                <v-template if="$odd">
                     <StackLayout style="padding: 50;">
                         <Label :text="line" style="color: red;"/>
                     </StackLayout>
                 </v-template>
             </list-view>
-        </StackLayout>
+        </GridLayout>
     </Page>
   `
 }).$start()
