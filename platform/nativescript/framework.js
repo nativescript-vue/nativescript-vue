14e5ed49b3759d67cfba39e26f01c7dd34134093
rigor789 <rigor789@gmail.com>
2017-12-22 17:35:16 +0000

    fix: don't color console output unless Vue.config.debug is true

--- platform/nativescript/framework.js
+++ platform/nativescript/framework.js
@@ -1,25 +1,9 @@
-import inspect from 'util-inspect'
-
-console.log = (function(log, inspect) {
-  return function() {
-    return log.apply(
-      this,
-      Array.prototype.map.call(arguments, function(arg) {
-        return inspect(arg, { depth: 1, colors: true })
-      })
-    )
-  }
-})(console.log, inspect)
-
-console.keys = function(object) {
-  console.log(Object.keys(object))
-}
-
 // This is required because some of the third party plugins rely on this
 // and cause errors since there is no process variable in {N}.
 global.process = global.process || {}
 global.process.env = global.process.env || {}
 
+import inspect from 'util-inspect'
 import Vue from './runtime/index'
 import ModalPlugin from './plugins/modal-plugin'
 import NavigatorPlugin from './plugins/navigator-plugin'
@@ -30,5 +14,20 @@ Vue.use(ModalPlugin)
 Vue.use(NavigatorPlugin)
 // Vue.use(DecoderPlugin)
 Vue.use(RouterPlugin)
+
+console.log = (function(log, inspect, Vue) {
+  return function() {
+    return log.apply(
+      this,
+      Array.prototype.map.call(arguments, function(arg) {
+        return inspect(arg, { depth: 1, colors: Vue.config.debug })
+      })
+    )
+  }
+})(console.log, inspect, Vue)
+
+console.keys = function(object) {
+  console.log(Object.keys(object))
+}
 
 export default Vue
