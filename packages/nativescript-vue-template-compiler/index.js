44f8f39cf58c68a9548c84380efa662be3c2a241
rigor789 <rigor789@gmail.com>
2017-12-23 15:18:27 +0000

    build: 0.7.3

--- packages/nativescript-vue-template-compiler/index.js
+++ packages/nativescript-vue-template-compiler/index.js
@@ -1,6 +1,6 @@
 
 /*!
- * NativeScript-Vue-Template-Compiler v0.7.2
+ * NativeScript-Vue-Template-Compiler v0.7.3
  * (Using Vue v2.5.13)
  * (c) 2017-2017 rigor789
  * Released under the MIT license.
