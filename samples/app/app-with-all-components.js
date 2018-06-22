f3033a3913b1e32e413a9da9e7f995a82a9ee9af
msaelices <msaelices@gmail.com>
2018-06-22 14:37:54 +0200

    feat (wrap ns components): Add DatePicker component to the app-with-all-components app

--- samples/app/app-with-all-components.js
+++ samples/app/app-with-all-components.js
@@ -4,23 +4,43 @@ Vue.config.debug = true
 Vue.config.silent = false
 
 new Vue({
-  data: {
-    labelCondition: false
+  data() {
+    return {
+      labelCondition: false,
+      selectedDate: new Date()
+    }
   },
   template: `
     <Frame>
       <Page>
         <ActionBar title="App with all components" />
 
-        <StackLayout>
-          <Label v-if="labelCondition" text="Enable" @tap="labelCondition = false"/>
-          <Label v-if="!labelCondition" text="Disable" @tap="labelCondition = true"/>
+        <StackLayout class="m-t-20">
+          <Label
+            v-if="labelCondition"
+            text="Label with labelCondition enabled. Tap me to disable"
+            textWrap
+            @tap="labelCondition = false"/>
 
+          <Label
+            v-if="!labelCondition"
+            text="Label with labelCondition disabled. Tap me to enable"
+            @tap="labelCondition = true"
+            textWrap />
+
+          <DatePicker
+            v-model="selectedDate"
+            @dateChange="onDateChanged" />
         </StackLayout>
       </Page>
     </Frame>
   `,
   created() {
     console.log(Vue.compile(this.$options.template).render.toString())
+  },
+  methods: {
+    onDateChanged() {
+      console.log(`Date changed to ${this.selectedDate}`)
+    }
   }
 }).$start()
