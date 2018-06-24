1b0cbe49e9202859d00fd588f10eac7838ecd578
msaelices <msaelices@gmail.com>
2018-06-24 09:34:49 +0200

    feat (wrap ns components): Add tabs to the app-with-all-components.js sample app and logs every $ref

--- samples/app/app-with-all-components.js
+++ samples/app/app-with-all-components.js
@@ -6,41 +6,51 @@ Vue.config.silent = false
 new Vue({
   data() {
     return {
+      activeTab: 0,
+      tabs: [
+        { key: 'form', title: 'Form' },
+        { key: 'list', title: 'List' },
+        { key: 'other', title: 'Other' },
+      ],
       timesPressed: 0,
       labelCondition: true,
-      selectedDate: new Date()
+      selectedDate: new Date(),
     }
   },
   template: `
-    <Frame>
-      <Page>
-        <ActionBar title="App with all components" />
-
-        <StackLayout class="m-t-20">
-          <Label
-            v-if="labelCondition"
-            text="Label with labelCondition enabled. Tap me to disable"
-            textWrap
-            @tap="labelCondition = false"/>
-
-          <Label
-            v-else
-            text="Label with labelCondition disabled. Tap me to enable"
-            @tap="labelCondition = true"
-            textWrap />
-
-          <DatePicker
-            ref="date"
-            v-model="selectedDate"
-            @dateChange="onDateChanged" />
-
-          <Button
-            :text="buttonText"
-            @tap="onButtonPress" />
-        </StackLayout>
-      </Page>
-    </Frame>
-  `,
+  <Page>
+    <ActionBar title="test">
+    </ActionBar>
+    <StackLayout>
+      <TabView v-model="activeTab">
+        <TabViewItem v-for="(tab, i) in tabs" :key="i + tab.title" :title="tab.title">
+          <StackLayout v-show="isTabActive('form')" class="tab">
+            <Label
+              v-if="labelCondition"
+              ref="label1"
+              style="margin-top: 10"
+              text="Label with labelCondition enabled. Tap me to disable"
+              textWrap
+              @tap="labelCondition = false"/>
+            <Label
+              v-else
+              ref="label2"
+              text="Label with labelCondition disabled. Tap me to enable"
+              @tap="labelCondition = true"
+              textWrap />
+            <DatePicker
+              ref="date"
+              v-model="selectedDate"
+              @dateChange="onDateChanged" />
+            <Button
+              :text="buttonText"
+              @tap="onButtonPress" />
+          </StackLayout>
+        </TabViewItem>
+      </TabView>
+    </StackLayout>
+  </Page>
+`,
   computed: {
     buttonText() {
       return this.timesPressed > 0
@@ -49,15 +59,24 @@ new Vue({
     }
   },
   methods: {
+    isTabActive(key) {
+      return this.tabs[this.activeTab].key === key
+    },
     onDateChanged() {
       console.log(`Date changed to ${this.selectedDate}`)
     },
     onButtonPress() {
       console.log('Button pressed')
       this.timesPressed++
-    }
+    },
   },
   created() {
     console.log(Vue.compile(this.$options.template).render.toString())
-  }
+  },
+  mounted() {
+    Object.keys(this.$refs).map((key) => {
+      console.dir(`this.$refs.${key} ->`)
+      console.dir(this.$refs[key])
+    })
+  },
 }).$start()
