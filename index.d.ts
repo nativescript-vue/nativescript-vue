7286e67c988a0433bd6ddf1a1dbe8c08e860894f
rigor789 <rigor789@gmail.com>
2018-07-04 10:13:35 +0200

    feat: include typedefinitions in npm package
    fix #262, fix #263

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,18 @@
 // Typings for NativeScript-Vue
 declare module 'nativescript-vue' {
-  // import vue.js typings
-  import Vue from 'vue';
+    // import vue.js typings
+    import Vue from 'vue';
 
-  // creat a nativescript vue class that extends vue.js
-  class NativeScriptVue extends Vue {
-    /**
-     * Registers NativeScript Plugin.
-     * @param elementName Name of the element to use in your template
-     * @param resolver  function to register the element
-     */
-    static registerElement(elementName: string, resolver: Function);
-  }
+    // creat a nativescript vue class that extends vue.js
+    class NativeScriptVue extends Vue {
+        /**
+         * Registers NativeScript Plugin.
+         * @param elementName Name of the element to use in your template
+         * @param resolver  function to register the element
+         * @param meta meta associated with the element
+         */
+        static registerElement(elementName: string, resolver: Function, meta?: any);
+    }
 
-  export = NativeScriptVue;
+    export = NativeScriptVue;
 }
