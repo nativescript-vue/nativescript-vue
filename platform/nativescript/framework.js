8e0e8d76b7982c50a15502bef85c9ba44d2a2973
Martin Guillon <martin.guillon@akylas.fr>
2019-03-04 10:34:10 +0100

    removed custom console log as it crashes the app when complex objects (like Vue components)

--- platform/nativescript/framework.js
+++ platform/nativescript/framework.js
@@ -1,9 +1,3 @@
-// This is required because some of the third party plugins rely on this
-// and cause errors since there is no process variable in {N}.
-global.process = global.process || {}
-global.process.env = global.process.env || {}
-
-import inspect from 'util-inspect'
 import { topmost } from 'tns-core-modules/ui/frame'
 import application from 'tns-core-modules/application'
 import Vue from './runtime/index'
@@ -18,27 +12,6 @@ setVue(Vue)
 
 Vue.use(ModalPlugin)
 Vue.use(NavigatorPlugin)
-
-const newLineRegExp = /\\n/g
-
-console.log = (function(log, inspect, Vue) {
-  return function(...args) {
-    return log.call(
-      this,
-      ...Array.prototype.map.call(args, function(arg) {
-        return inspect(arg, {
-          depth: 2,
-          colors: Vue.config.debug,
-          showHidden: true
-        }).replace(newLineRegExp, '\n')
-      })
-    )
-  }
-})(console.log, inspect, Vue)
-
-console.keys = function(object) {
-  console.log(Object.keys(object))
-}
 
 // this fixes the issue of resuming the application
 // however this might not be the desired functionality
