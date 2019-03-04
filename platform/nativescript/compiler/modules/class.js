7e8768e0434bd0a77b44ae569c9d6612fc802b10
Martin Guillon <martin.guillon@akylas.fr>
2019-03-04 10:33:24 +0100

    removed deprecated test. Faster as now regex testing

--- platform/nativescript/compiler/modules/class.js
+++ platform/nativescript/compiler/modules/class.js
@@ -4,17 +4,17 @@ import { parseText } from 'compiler/parser/text-parser'
 function transformNode(el, options) {
   const warn = options.warn || baseWarn
   const staticClass = getAndRemoveAttr(el, 'class')
-  if (process.env.NODE_ENV !== 'production' && staticClass) {
-    const expression = parseText(staticClass, options.delimiters)
-    if (expression) {
-      warn(
-        `class="${staticClass}": ` +
-          'Interpolation inside attributes has been removed. ' +
-          'Use v-bind or the colon shorthand instead. For example, ' +
-          'instead of <div class="{{ val }}">, use <div :class="val">.'
-      )
-    }
-  }
+  // if (process.env.NODE_ENV !== 'production' && staticClass) {
+  //   const expression = parseText(staticClass, options.delimiters)
+  //   if (expression) {
+  //     warn(
+  //       `class="${staticClass}": ` +
+  //         'Interpolation inside attributes has been removed. ' +
+  //         'Use v-bind or the colon shorthand instead. For example, ' +
+  //         'instead of <div class="{{ val }}">, use <div :class="val">.'
+  //     )
+  //   }
+  // }
   if (staticClass) {
     el.staticClass = JSON.stringify(staticClass)
   }
