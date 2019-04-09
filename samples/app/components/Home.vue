2387968c7182d3cfc04c112ec4c42d0b3cba3db0
Manuel Saelices <msaelices@gmail.com>
2019-04-09 16:24:40 +0200

    feat (samples): Add a sample with a Vue component to check the HMR feature.

---
+++ samples/app/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <Page>
+    <ActionBar class="action-bar" title="Home Page">
+    </ActionBar>
+    <StackLayout>
+      <Button text="Open page" @tap="openPage" />
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+const DetailsPage = {
+  template: `
+  <Page>
+    <ActionBar class="action-bar" title="Details Page">
+      <ActionItem text="Action"></ActionItem>
+    </ActionBar>
+    <StackLayout>
+      <Label :text="'Details ' + Math.random()" />
+      <Button text="another" @tap="openDetails" />
+      <Button text="back" @tap="goBack" />
+    </StackLayout>
+  </Page>
+  `,
+  methods: {
+    openDetails() {
+      this.$navigateTo(DetailsPage)
+    },
+    goBack() {
+      this.$navigateBack()
+    }
+  }
+}
+
+export default {
+  components: [
+    DetailsPage,
+  ],
+  methods: {
+    openPage() {
+      this.$navigateTo(DetailsPage)
+    }
+  }
+}
+</script>
