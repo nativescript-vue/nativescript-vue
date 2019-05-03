1dda9a1636d12be0db7869fff0d922686c77554c
Manuel Saelices <msaelices@gmail.com>
2019-05-03 11:07:15 +0200

    fix (tests): More changes due to the element-registry refactoring

--- platform/nativescript/compiler.js
+++ platform/nativescript/compiler.js
@@ -1,3 +1,3 @@
 export { parseComponent } from './compiler/sfc/parser'
 export { compile, compileToFunctions } from './compiler/index'
-export { registerElement } from './element-registry'
+export { registerElement } from './register'
