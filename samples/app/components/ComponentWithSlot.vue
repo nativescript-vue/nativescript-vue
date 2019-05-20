45af86bd98de1c1a9a10f24fb8bd34aead7a0a02
Manuel Saelices <msaelices@gmail.com>
2019-05-20 17:50:15 +0200

    Samples refactoring to use a bundle app with webpack and support HMR and .vue files (#476)
    * feat (samples): Run the samples using a webpack bundle. Closer to real-world NS-vue apps. Allow any aditional parameters to the tns command like --hmr or whatever.
    
    * feat (samples): Add a sample with a Vue component to check the HMR feature.
    
    * Change NPM dev script to build to dist/index.js. Remove the dev:dist script as it is not useful now. Document new features
    
    * feat (samples): Fix live reload when changing the NS-vue code
    
    * feat (sample): Adapt all the import to nativescript-vue
    
    * feat (samples): Separate the details page into a new .vue component for better UX
    
    * fix (samples): Fix the 455.js sample which is now adapted to the new samples infrastructure
    
    * chore (v-slot): Add sample (currently failing) for testing v-slot in a .vue component

---
+++ samples/app/components/ComponentWithSlot.vue
@@ -0,0 +1,16 @@
+<template>
+  <StackLayout :color="counter < 5 ? 'red' : 'blue'">
+    <slot :counter="counter"/>
+    <Button text="+1" @tap="counter++"/>
+  </StackLayout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+}
+</script>
---
+++ samples/app/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <Page>
+    <ActionBar class="action-bar" title="Home Page">
+    </ActionBar>
+    <StackLayout>
+      <Button text="Open page" @tap="openPage" />
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+import Details from './Details'
+
+export default {
+  components: [
+    Details,
+  ],
+  methods: {
+    openPage() {
+      this.$navigateTo(Details)
+    }
+  }
+}
+</script>
