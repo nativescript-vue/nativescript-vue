9258d4412f1b72275ffc09c36c7bad530b41d005
DrunkenPoney <mrquebec13@gmail.com>
2019-05-24 14:43:54 -0400

    fix(samples): add sample for issue #464
    ref #464

---
+++ samples/app/464.js
@@ -0,0 +1,31 @@
+const Vue = require('./nativescript-vue')
+const {
+  ObservableArray
+} = require('tns-core-modules/data/observable-array/observable-array')
+
+Vue.config.debug = true
+Vue.config.silent = false
+
+new Vue({
+  data: {
+    items: new ObservableArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  },
+  methods: {
+    onItemTap({ item }) {
+      this.items.push(`Item clicked: ${item} (shouldn't be \`undefined\`)`)
+    }
+  },
+  template: `
+    <Frame>
+      <Page class="page">
+        <ActionBar title="Home" class="action-bar" />
+        <ScrollView>
+          <ListView for="item in items" @itemTap="onItemTap">
+            <v-template>
+              <Label :text="item"/>
+            </v-template>
+          </ListView>
+        </ScrollView>
+      </Page>
+    </Frame>`
+}).$start()
