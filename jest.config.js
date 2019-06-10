448b25594b01e14dd7be15bf10f155d5ba786b01
Manuel Saelices <msaelices@gmail.com>
2019-06-10 20:19:58 +0200

    refactor (tests): fixing some issues running jest. Other pending. Work In Progress

---
+++ jest.config.js
@@ -0,0 +1,49 @@
+const { readFileSync } = require("fs")
+const babelConfig = JSON.parse(readFileSync("./.babelrc", "utf8"))
+
+require("@babel/register")(babelConfig)
+require("@babel/polyfill")
+
+const { join } = require("path")
+const ROOT = process.cwd()
+const TESTS = join(ROOT, "tests")
+
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "vue"
+  ],
+  modulePaths: [
+    "<rootDir>/platform/nativescript",
+    "<rootDir>/node_modules/vue/src",
+    "<rootDir>/node_modules/vue/src/platforms"
+  ],
+  collectCoverageFrom: [
+    "platform/**/*.js",
+    "!**/node_modules/**"
+  ],
+  moduleDirectories: [
+    "node_modules"
+  ],
+  moduleNameMapper: {
+    "^compiler/(.*)": "<rootDir>/node_modules/vue/src/compiler/$1",
+    "^core/(.*)": "<rootDir>/node_modules/vue/src/core/$1"
+  },
+  modulePathIgnorePatterns: [
+    "<rootDir>/samples"
+  ],
+  collectCoverage: true,
+  testURL: "http://localhost",
+  testMatch: [
+    "**/tests/**/*.spec.(js|jsx|ts)"
+  ],
+  transform: {
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
+  },
+  setupFiles: [
+    join(TESTS, "jest-setup.js")
+  ],
+}
