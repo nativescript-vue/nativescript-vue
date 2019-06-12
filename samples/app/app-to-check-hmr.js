d9ce8c25cfa18a82ee5759ab1473eb09c4c72849
rigor789 <rigor789@gmail.com>
2019-06-12 20:45:31 +0200

    fix: appium local

--- samples/app/app-to-check-hmr.js
+++ samples/app/app-to-check-hmr.js
@@ -1,6 +1,4 @@
 const Vue = require('nativescript-vue')
-const application = require('tns-core-modules/application')
-const platform = require('tns-core-modules/platform')
 
 Vue.config.silent = false
 Vue.config.debug = true
