1cc8142222c29f6dfb83b9b771590057cfac0d65
Muammer YILMAZ <yilmaz.muammer@outlook.com>
2019-09-11 11:12:27 +0300

    fix(router module): safely check parent tagname. fixes #542 (#550)

--- platform/nativescript/compiler/modules/router.js
+++ platform/nativescript/compiler/modules/router.js
@@ -3,7 +3,11 @@ import { addAttr } from 'compiler/helpers'
 
 function preTransformNode(el) {
   if (el.tag !== 'router-view') return
-  if (normalizeElementName(el.parent.tag) === 'nativeframe') {
+  if (
+    el.parent &&
+    el.parent.tag &&
+    normalizeElementName(el.parent.tag) === 'nativeframe'
+  ) {
     addAttr(el.parent, 'hasRouterView', 'true')
   }
 }
