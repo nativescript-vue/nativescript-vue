f48e80afd5e8cf44a8ca1d01065b989729e948c3
Manuel Saelices <msaelices@gmail.com>
2019-10-03 19:49:59 +0200

    chore: add sample app which reproduces the #536 issue

---
+++ samples/app/536.js
@@ -0,0 +1,124 @@
+import Vue from 'nativescript-vue'
+
+Vue.config.debug = true
+Vue.config.silent = false
+
+const Step2 = {
+  template: `
+    <Page>
+      <ActionBar title="Step 2" />
+
+      <GridLayout rows="*, auto, auto">
+        <Label text="This is the second step of the wizard." row="0" />
+
+        <Button text="Previous" @tap="prevStep" row="1" />
+      </GridLayout>
+    </Page>
+  `,
+  methods: {
+    prevStep() {
+      this.$navigateBack({
+        frame: 'wizard'
+      })
+    }
+  }
+}
+
+const WizardModal = {
+  template: `
+    <Frame id="wizard">
+      <Page>
+        <ActionBar title="Step 1" />
+        <GridLayout rows="*, auto">
+          <Label text="This is the first step of the wizard." row="0" />
+          <Button text="Next" @tap="nextStep" row="1" />
+        </GridLayout>
+      </Page>
+    </Frame>
+  `,
+  methods: {
+    nextStep() {
+      this.$navigateTo(Step2, {
+        frame: 'wizard'
+      })
+    }
+  }
+}
+
+const TabContent = {
+  template: `
+  <GridLayout rows="auto, auto">
+      <Label text="This is the home page." row="0" />
+      <Button text="Open Wizard" row="1" @tap="openWizard" />
+  </GridLayout>
+  `,
+  methods: {
+    openWizard() {
+      // show the wizard in a modal, and make sure it is fullscreen.
+      this.$showModal(WizardModal, {
+        fullscreen: true
+      }).then(res => {
+        console.log('wizard completed with res', res)
+      })
+    }
+  }
+}
+
+new Vue({
+  components: {
+    TabContent
+  },
+  template: `
+  <GridLayout rows="*">
+    <TabView androidTabsPosition="bottom" iosIconRenderingMode="alwaysOriginal">
+      <TabViewItem title="Tab1" textTransform="capitalize">
+        <Frame id="main-frame">
+          <Page class="page">
+            <ActionBar title="#536" />
+            <TabContent />
+          </Page>
+        </Frame>
+      </TabViewItem>
+    </TabView>
+  </GridLayout>
+  `
+}).$start()
+
+// Working code. Please comment the previous Vue instance and
+// uncomment the next one in order to workaround the issue
+// The workaround is not using a new Vue component as the
+// Tab content, so the wizard is opened using the root component
+/*
+new Vue({
+  components: {
+    TabContent,
+  },
+  template: `
+  <GridLayout rows="*">
+    <TabView androidTabsPosition="bottom" iosIconRenderingMode="alwaysOriginal">
+      <TabViewItem title="Tab1" textTransform="capitalize">
+        <Frame id="main-frame">
+          <Page class="page">
+            <ActionBar title="#536 - workaround" />
+            <GridLayout rows="auto, auto">
+              <Label text="This is the home page." row="0" />
+              <Button text="Open Wizard" row="1" @tap="openWizard" />
+            </GridLayout>
+          </Page>
+        </Frame>
+      </TabViewItem>
+    </TabView>
+  </GridLayout>
+  `,
+  methods: {
+    openWizard() {
+      // show the wizard in a modal, and make sure it is fullscreen.
+      this.$showModal(WizardModal, {
+        fullscreen: true
+      }).then(res => {
+        console.log('wizard completed with res', res);
+      });
+    }
+  },
+}).$start()
+*/
