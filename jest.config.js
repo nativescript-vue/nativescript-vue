892654ae35a8c453608441b16a38b15dcef7a8e3
Manuel Saelices <msaelices@gmail.com>
2020-01-27 18:14:32 +0100

    feat (tests): update the jest related packages to the latest version

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ const { readFileSync } = require("fs")
 const babelConfig = JSON.parse(readFileSync("./.babelrc", "utf8"))
 
 require("@babel/register")(babelConfig)
-require("@babel/polyfill")
 
 const { join } = require("path")
 const ROOT = process.cwd()
