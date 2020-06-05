b20d68a45930d39ef8adc962a5c6b52541f0978d
bblake <bblake@goempyrean.com>
2020-06-05 14:27:15 -0500

    fix: adding backstackentry to navigateback type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,8 @@
-import { Page, NavigationEntry } from 'tns-core-modules/ui/frame/frame'
+import {
+    Page,
+    NavigationEntry,
+    BackstackEntry,
+} from 'tns-core-modules/ui/frame/frame'
 import { ItemEventData } from 'tns-core-modules/ui/list-view'
 import { View } from 'tns-core-modules/ui/core/view'
 import { ShowModalOptions } from 'tns-core-modules/ui/core/view-base'
@@ -8,58 +12,68 @@ import { Vue, VueConstructor } from 'vue/types/vue'
 export type NativeScriptVueItemEventData<T> = ItemEventData & { item: T }
 
 export interface NavigationEntryVue extends NavigationEntry {
-    props?: Record<string, any>,
-    frame?: any,
+    props?: Record<string, any>
+    frame?: any
     resolveOnEvent?: Page.navigatingToEvent | Page.navigatedToEvent
 }
 
 export type navigateTo = (
     component: VueConstructor,
     options?: NavigationEntryVue,
-    cb?: () => Page,
-) => Promise<Page>;
+    cb?: () => Page
+) => Promise<Page>
+
+export type navigateBack = (
+    backstackEntry: BackstackEntry,
+    options?: NavigationEntryVue,
+) => void
 
 export interface ModalOptions extends Partial<ShowModalOptions> {
-    target?: any; // optional Vue target to open the modal from
-    props?: Record<string, any>;
+    target?: any // optional Vue target to open the modal from
+    props?: Record<string, any>
 }
 
 // create a nativescript vue class that extends vue.js
 export interface NativeScriptVue<V = View> extends Vue {
-    nativeView: V
+nativeView: V
 
-    $navigateTo: navigateTo
-    $navigateBack: () => void
+$navigateTo: navigateTo
+$navigateBack: navigateBack
 
-    $modal?: { close: (data?: any) => Promise<typeof data> };
+$modal?: { close: (data?: any) => Promise<typeof data> }
 
-    /**
-     * Open a modal using a component
-     * @param {typeof Vue} component
-     * @param {ModalOptions} options
-     * @returns {any}
-     */
-    $showModal: (component: typeof Vue, options?: ModalOptions) => Promise<any>;
+/**
+ * Open a modal using a component
+ * @param {typeof Vue} component
+ * @param {ModalOptions} options
+ * @returns {any}
+ */
+$showModal: (component: typeof Vue, options?: ModalOptions) => Promise<any>
 
-    /**
-     * starts the nativescript application
-     */
-    $start: () => void
+/**
+ * starts the nativescript application
+ */
+$start: () => void
 }
 
-export interface NativeScriptVueConstructor extends VueConstructor<NativeScriptVue> {
-    navigateTo: navigateTo
-    navigateBack: () => void
-    /**
-     * Registers NativeScript Plugin.
-     * @param elementName Name of the element to use in your template
-     * @param resolver  function to register the element
-     * @param meta meta associated with the element
-     */
-    registerElement: (elementName: string, resolver: Function, meta?: any) => void
+export interface NativeScriptVueConstructor
+extends VueConstructor<NativeScriptVue> {
+navigateTo: navigateTo
+navigateBack: navigateBack
+/**
+ * Registers NativeScript Plugin.
+ * @param elementName Name of the element to use in your template
+ * @param resolver  function to register the element
+ * @param meta meta associated with the element
+ */
+registerElement: (
+    elementName: string,
+    resolver: Function,
+    meta?: any
+) => void
 }
 
 export const NativeScriptVue: NativeScriptVueConstructor
 
 // export as namespace NativeScriptVue;
-export default NativeScriptVue;
+export default NativeScriptVue
