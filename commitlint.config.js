274e44f53fbf34df48c78230825b04e50133b447
Igor Randjelovic <rigor789@gmail.com>
2026-09-14 11:32:29 +0200

    release: 3.1.0 (#1155)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // `npm run release` commits as `release: <version>`
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'release',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
 };
